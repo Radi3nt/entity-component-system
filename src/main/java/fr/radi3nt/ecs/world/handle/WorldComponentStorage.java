f443a355efbe0c125804f2cdb0b7cfcae3abda98
Radi3nt <pro.radi3nt@gmail.com>
2024-05-05 16:17:26 +0200

    Refactored how entities, components and systems interact

---
+++ src/main/java/fr/radi3nt/ecs/world/handle/WorldComponentStorage.java
@@ -0,0 +1,57 @@
+package fr.radi3nt.ecs.world.handle;
+
+import fr.radi3nt.ecs.components.Component;
+import fr.radi3nt.ecs.world.listener.ComponentListener;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class WorldComponentStorage {
+
+    private final Map<Class<? extends Component>, Collection<Component>> allComponents = new ConcurrentHashMap<>();
+    private final Map<Class<? extends Component>, Collection<ComponentListener>> listeners = new ConcurrentHashMap<>();
+
+    public void addComponent(Component component) {
+        Collection<Component> allComponentOfThatType = allComponents.computeIfAbsent(component.getClass(), aClass -> new ArrayList<>());
+        allComponentOfThatType.add(component);
+
+        notifyAddListeners(component);
+    }
+
+    private void notifyAddListeners(Component component) {
+        Collection<ComponentListener> componentListeners = listeners.get(component.getClass());
+        if (componentListeners==null)
+            return;
+        for (ComponentListener componentListener : componentListeners) {
+            componentListener.added(component);
+        }
+    }
+
+    public void removeComponent(Component component) {
+        Collection<Component> components = allComponents.get(component.getClass());
+        if (components!=null) {
+            components.remove(component);
+        }
+
+        notifyRemoveListeners(component);
+    }
+
+    private void notifyRemoveListeners(Component component) {
+        Collection<ComponentListener> componentListeners = listeners.get(component.getClass());
+        if (componentListeners==null)
+            return;
+        for (ComponentListener componentListener : componentListeners) {
+            componentListener.removed(component);
+        }
+    }
+
+    public Map<Class<? extends Component>, Collection<Component>> getAllComponents() {
+        return allComponents;
+    }
+
+    public Map<Class<? extends Component>, Collection<ComponentListener>> getListeners() {
+        return listeners;
+    }
+}
